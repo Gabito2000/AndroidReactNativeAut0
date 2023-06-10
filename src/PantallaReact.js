ee6d0876309af7925d5022e371fadf00539469fa
Gabriel Kryger <gabrielkrygermesa@gmail.com>
2023-06-10 01:00:15 -0300

    pronta app android faltan req

--- src/PantallaReact.js
+++ src/PantallaReact.js
@@ -1,24 +1,282 @@
 import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, FlatList, TouchableOpacity, SafeAreaView, ScrollView, RefreshControl } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { LocationSender } from './LocationSender';
-const trips = [
+
+const idChofer = 1;
+
+let trips = [
   {
-    id: 1,
-    destination: 'Trip 1',
-    details: 'Trip 1 details',
+    "guia_viaje": {
+      "destino": "Colonia",
+      "fechaHora": "17/09/2021 10:00",
+      "id": 1,
+      "idViajes": [17],
+      "origen": "Montevideo",
+      "rubroCarga": "Carga general",
+      "tipoCarga": "Carga general",
+      "volumenCarga": 10.17
+    },
+    "viaje": {
+      "estado": "EN_CURSO",
+      "fechaHoraFin": null,
+      "fechaHoraInicio": "17/09/2021 10:00",
+      "id": 17,
+      "idChoferes": [1],
+      "idGuiaViaje": 1,
+      "idVehiculo": 1
+    }
   },
   {
-    id: 2,
-    destination: 'Trip 2',
-    details: 'Trip 2 details',
+    "guia_viaje": {
+      "destino": "Buenos Aires",
+      "fechaHora": "20/09/2021 14:30",
+      "id": 2,
+      "idViajes": [21],
+      "origen": "Montevideo",
+      "rubroCarga": "Carga general",
+      "tipoCarga": "Carga general",
+      "volumenCarga": 15.8
+    },
+    "viaje": {
+      "estado": "EN_CURSO",
+      "fechaHoraFin": null,
+      "fechaHoraInicio": "20/09/2021 14:30",
+      "id": 21,
+      "idChoferes": [1],
+      "idGuiaViaje": 2,
+      "idVehiculo": 3
+    }
   },
   {
-    id: 3,
-    destination: 'Trip 3',
-    details: 'Trip 3 details',
+    "guia_viaje": {
+      "destino": "Punta del Este",
+      "fechaHora": "25/09/2021 08:00",
+      "id": 3,
+      "idViajes": [31],
+      "origen": "Montevideo",
+      "rubroCarga": "Carga general",
+      "tipoCarga": "Carga general",
+      "volumenCarga": 7.2
+    },
+    "viaje": {
+      "estado": "EN_CURSO",
+      "fechaHoraFin": null,
+      "fechaHoraInicio": "25/09/2021 08:00",
+      "id": 31,
+      "idChoferes": [1],
+      "idGuiaViaje": 3,
+      "idVehiculo": 2
+    }
+  },
+  {
+    "guia_viaje": {
+      "destino": "Salto",
+      "fechaHora": "30/09/2021 12:15",
+      "id": 4,
+      "idViajes": [42],
+      "origen": "Montevideo",
+      "rubroCarga": "Carga general",
+      "tipoCarga": "Carga general",
+      "volumenCarga": 12.5
+    },
+    "viaje": {
+      "estado": "EN_CURSO",
+      "fechaHoraFin": null,
+      "fechaHoraInicio": "30/09/2021 12:15",
+      "id": 42,
+      "idChoferes": [1],
+      "idGuiaViaje": 4,
+      "idVehiculo": 5
+    }
+  },
+  {
+    "guia_viaje": {
+      "destino": "Rivera",
+      "fechaHora": "05/10/2021 16:45",
+      "id": 5,
+      "idViajes": [52],
+      "origen": "Montevideo",
+      "rubroCarga": "Carga general",
+      "tipoCarga": "Carga general",
+      "volumenCarga": 9.3
+    },
+    "viaje": {
+      "estado": "EN_CURSO",
+      "fechaHoraFin": null,
+      "fechaHoraInicio": "05/10/2021 16:45",
+      "id": 52,
+      "idChoferes": [5],
+      "idGuiaViaje": 5,
+      "idVehiculo": 4
+    }
+  },
+  // Additional items
+  {
+    "guia_viaje": {
+      "destino": "Tacuarembó",
+      "fechaHora": "10/10/2021 09:30",
+      "id": 6,
+      "idViajes": [61],
+      "origen": "Montevideo",
+      "rubroCarga": "Carga general",
+      "tipoCarga": "Carga general",
+      "volumenCarga": 8.7
+    },
+    "viaje": {
+      "estado": "EN_CURSO",
+      "fechaHoraFin": null,
+      "fechaHoraInicio": "10/10/2021 09:30",
+      "id": 61,
+      "idChoferes": [3],
+      "idGuiaViaje": 6,
+      "idVehiculo": 2
+    }
+  },
+  {
+    "guia_viaje": {
+      "destino": "Maldonado",
+      "fechaHora": "15/10/2021 14:45",
+      "id": 7,
+      "idViajes": [73],
+      "origen": "Montevideo",
+      "rubroCarga": "Carga general",
+      "tipoCarga": "Carga general",
+      "volumenCarga": 11.2
+    },
+    "viaje": {
+      "estado": "EN_CURSO",
+      "fechaHoraFin": null,
+      "fechaHoraInicio": "15/10/2021 14:45",
+      "id": 73,
+      "idChoferes": [2],
+      "idGuiaViaje": 7,
+      "idVehiculo": 1
+    }
+  },
+  {
+    "guia_viaje": {
+      "destino": "Artigas",
+      "fechaHora": "20/10/2021 11:30",
+      "id": 8,
+      "idViajes": [85],
+      "origen": "Montevideo",
+      "rubroCarga": "Carga general",
+      "tipoCarga": "Carga general",
+      "volumenCarga": 6.5
+    },
+    "viaje": {
+      "estado": "EN_CURSO",
+      "fechaHoraFin": null,
+      "fechaHoraInicio": "20/10/2021 11:30",
+      "id": 85,
+      "idChoferes": [4],
+      "idGuiaViaje": 8,
+      "idVehiculo": 3
+    }
+  },
+  {
+    "guia_viaje": {
+      "destino": "Treinta y Tres",
+      "fechaHora": "25/10/2021 16:15",
+      "id": 9,
+      "idViajes": [92],
+      "origen": "Montevideo",
+      "rubroCarga": "Carga general",
+      "tipoCarga": "Carga general",
+      "volumenCarga": 13.1
+    },
+    "viaje": {
+      "estado": "EN_CURSO",
+      "fechaHoraFin": null,
+      "fechaHoraInicio": "25/10/2021 16:15",
+      "id": 92,
+      "idChoferes": [5],
+      "idGuiaViaje": 9,
+      "idVehiculo": 4
+    }
+  },
+  {
+    "guia_viaje": {
+      "destino": "Rocha",
+      "fechaHora": "30/10/2021 13:45",
+      "id": 10,
+      "idViajes": [105],
+      "origen": "Montevideo",
+      "rubroCarga": "Carga general",
+      "tipoCarga": "Carga general",
+      "volumenCarga": 9.8
+    },
+    "viaje": {
+      "estado": "EN_CURSO",
+      "fechaHoraFin": null,
+      "fechaHoraInicio": "30/10/2021 13:45",
+      "id": 105,
+      "idChoferes": [3],
+      "idGuiaViaje": 10,
+      "idVehiculo": 5
+    }
+  },
+  {
+    "guia_viaje": {
+      "destino": "Durazno",
+      "fechaHora": "05/11/2021 09:00",
+      "id": 11,
+      "idViajes": [118],
+      "origen": "Montevideo",
+      "rubroCarga": "Carga general",
+      "tipoCarga": "Carga general",
+      "volumenCarga": 7.9
+    },
+    "viaje": {
+      "estado": "EN_CURSO",
+      "fechaHoraFin": null,
+      "fechaHoraInicio": "05/11/2021 09:00",
+      "id": 118,
+      "idChoferes": [2],
+      "idGuiaViaje": 11,
+      "idVehiculo": 1
+    }
+  },
+  {
+    "guia_viaje": {
+      "destino": "Canelones",
+      "fechaHora": "10/11/2021 15:30",
+      "id": 12,
+      "idViajes": [122],
+      "origen": "Montevideo",
+      "rubroCarga": "Carga general",
+      "tipoCarga": "Carga general",
+      "volumenCarga": 10.5
+    },
+    "viaje": {
+      "estado": "EN_CURSO",
+      "fechaHoraFin": null,
+      "fechaHoraInicio": "10/11/2021 15:30",
+      "id": 122,
+      "idChoferes": [4],
+      "idGuiaViaje": 12,
+      "idVehiculo": 3
+    }
   },
 ];
+
+// trips = trips.filter((trip) => trip.viaje.idChoferes.includes(idChofer));
+
+const returnViajesFromGuiaViajes = () => {
+  const viajes = new Map();
+  trips.forEach((trip) => {
+    if (viajes.get(trip.guia_viaje.id) === undefined) {
+      viajes.set(trip.guia_viaje.id, new Map());
+    }
+    viajes.get(trip.guia_viaje.id).set(trip.viaje.id, trip.viaje);
+  });
+  return viajes;
+};
+
+const guia_viajes = trips.map((trip) => trip.guia_viaje);
+
+const viajes = returnViajesFromGuiaViajes();
 
 export const PantallaReact = () => {
   const [selectedTrip, setSelectedTrip] = useState(null);
@@ -43,56 +301,83 @@ export const PantallaReact = () => {
   const renderTripDetails = () => {
     if (!selectedTrip) return null;
 
-    const trip = trips.find((t) => t.id === selectedTrip);
+    const trip = trips.find((t) => t.viaje.id === selectedTrip);
 
     return (
       <View style={styles.tripDetailsContainer}>
         <Text style={styles.tripDetailsText}>Trip Details:</Text>
-        <Text style={styles.tripDetailsText}>Destination: {trip.destination}</Text>
-        <Text style={styles.tripDetailsText}>Details: {trip.details}</Text>
+        <Text style={styles.tripDetailsText}>Origin: {trip.guia_viaje.origen}</Text>
+        <Text style={styles.tripDetailsText}>Destination: {trip.guia_viaje.destino}</Text>
+        <Text style={styles.tripDetailsText}>Date: {trip.guia_viaje.fechaHora}</Text>
+        <Text style={styles.tripDetailsText}>Type: {trip.guia_viaje.tipoCarga}</Text>
+        <Text style={styles.tripDetailsText}>Category: {trip.guia_viaje.rubroCarga}</Text>
+        <Text style={styles.tripDetailsText}>Volume: {trip.guia_viaje.volumenCarga}</Text>
       </View>
     );
   };
 
-  return (
-    <View style={styles.container}>
+  const onRefresh = console.log('refreshing');
 
-      { !tripStarted && (
-        <>
-        <Text style={styles.heading}>Select a Trip:</Text>
-          <Picker
-            selectedValue={selectedTrip}
-            onValueChange={(itemValue) => setSelectedTrip(itemValue)}
-            style={styles.picker}
-          >
-            <Picker.Item label="Select a trip" value={null} />
-            {trips.map((trip) => (
-              <Picker.Item key={trip.id} label={trip.destination} value={trip.id} />
-            ))}
-          </Picker>
-        </>
-       )
-      }
-      {selectedTrip && (
-        <View style={styles.tripContainer}>
-          {renderTripDetails()}
-          {tripStarted ? (
-            <>
-              <Text>Trip Started!</Text>
-              <LocationSender idViaje = {selectedTrip} />
+  const refreshing = false;
+
+  return (
+    <ScrollView
+        contentContainerStyle={styles.scrollView}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }>
+      <View style={styles.container}>
+        {!tripStarted && (
+          <>
+            <Text style={styles.heading}>Select a Trip:</Text>
+            <SafeAreaView style={styles.tableContainer}>
+              <View style={styles.tableRow}>
+                <Text style={styles.tableHeader}>Origin</Text>
+                <Text style={styles.tableHeader}>Destination</Text>
+                <Text style={styles.tableHeader}>Date</Text>
+                <Text style={styles.tableHeader}>Trip ID</Text>
+                <Text style={styles.tableHeader}>Status</Text>
+              </View>
+              {trips.map((item) => {return (
+                  <TouchableOpacity key={item.viaje.id} onPress={() => {item.viaje.id === selectedTrip ? setSelectedTrip(null) : setSelectedTrip(item.viaje.id)}}>
+                    <View style={styles.tableRow}>
+                      <Text style={styles.tableCell}>{`${item.guia_viaje.origen}`}</Text>
+                      <Text style={styles.tableCell}>{`${item.guia_viaje.destino}`}</Text>
+                      <Text style={styles.tableCell}>{`${item.guia_viaje.fechaHora}`}</Text>
+                      <Text style={styles.tableCell}>{`${item.viaje.id}`}</Text>
+                      <Text style={styles.tableCell}>{`${item.viaje.estado}`}</Text>
+                    </View>
+                    {selectedTrip && (selectedTrip == item.viaje.id) && (
+                      <View style={styles.tripContainer}>
+                        {renderTripDetails()}
+                        <Button title="Start Trip" onPress={handleStartTrip} />
+                      </View>
+                    )}
+                    {/* add black line */}
+                    <View style={{ height: 1, backgroundColor: 'black' }} />
+                  </TouchableOpacity>
+              )})}
+            </SafeAreaView>
+          </>
+        )}
+        {tripStarted && (
+          <View style={styles.tripContainer}>
+            {renderTripDetails()}
+            <Text>Trip Started!</Text>
+            <LocationSender idViaje={selectedTrip} />
+            <View style={styles.buttonContainer}>
               <View style={styles.buttonContainer}>
                 <Button title="Cancel Trip" onPress={handleCancelTrip} />
               </View>
               <View style={styles.buttonContainer}>
                 <Button title="End Trip" onPress={handleEndTrip} />
               </View>
-            </>
-          ) : (
-            <Button title="Start Trip" onPress={handleStartTrip} />
-          )}
-        </View>
-      )}
-    </View>
+            </View>
+          </View>
+        )}
+        
+      </View>
+    </ScrollView>
   );
 };
 
@@ -107,9 +392,18 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginBottom: 20,
   },
-  picker: {
-    width: 200,
+  tableContainer: {
     marginBottom: 20,
+    width: '100%',
+  },
+  tableRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+  tableCell: {
+    flex: 1,
+    textAlign: 'center',
   },
   tripContainer: {
     alignItems: 'center',
@@ -125,5 +419,8 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
     padding: 10,
+  },
+  titleCell: {
+    fontWeight: 'bold',
   },
 });
